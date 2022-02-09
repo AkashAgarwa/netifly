10538126475e4549c2b627655efc98df35a37d37
AkashAgarwa <97722279+AkashAgarwa@users.noreply.github.com>
2022-02-08 18:06:55 -0800

    Add files via upload

---
+++ src/component/VaultAppRoles.js
@@ -0,0 +1,5 @@
+function VaultAppRoles() {
+    return (  <div style={{    backgroundColor: "#1d212c" , height:"calc( 100vh - 4.375rem )"}}><h1>Hello</h1></div>);
+}
+
+export default VaultAppRoles;
